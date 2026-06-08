b9061310a167f353b38c5f45beaaf454937c4142
jerom-larens <jeromelarens5@gmail.com>
2026-06-08 11:14:31 +0530

    Fix SMTP config

--- backend/server.js
+++ backend/server.js
@@ -48,11 +48,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  tls: {
-    rejectUnauthorized: false,
-  },
 });
-
 /* ========================
    Send OTP
 ======================== */
